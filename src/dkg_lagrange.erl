e278659680b07bfd11723c79532fe4c866395469
Rahul Garg <vihu89@gmail.com>
2018-05-31 10:40:54 -0700

    WIP, math for lagrange interpolation, not inline with the cpp code

--- src/dkg_lagrange.erl
+++ src/dkg_lagrange.erl
@@ -1,3 +1,29 @@
 -module(dkg_lagrange).
 
+-export([interpolate/2, apply/2]).
 
+%% interpolate should give back a lagrange polynomial
+interpolate(Pairing, Ac) ->
+    %% Ac = [{m0, a0}, {m1, a1}, {m2, a2}]
+    Ms = [M_j || {M_j, _} <- Ac],
+    Zero = erlang_pbc:element_set(erlang_pbc:element_new('Zr', Pairing), 0),
+    BasisPolys = [ erlang_pbc:element_mul(Alpha_j, basis_polynomial(Pairing, Ms, J)) || {J, {_, Alpha_j}} <- enumerate(Ac) ],
+    lists:foldl(fun(V, Acc) ->
+                        erlang_pbc:element_add(Acc, V)
+                end, Zero, BasisPolys).
+
+basis_polynomial(Pairing, Ms, J) ->
+    One = erlang_pbc:element_set(erlang_pbc:element_new('Zr', Pairing), 1),
+    Num = lists:foldl(fun(E, Acc) ->
+                              erlang_pbc:element_mul(Acc, E)
+                      end, One, [ 0 - M  - 1 || M <- Ms, M /= J]),
+    Den = lists:foldl(fun(E, Acc) ->
+                              erlang_pbc:element_mul(Acc, E)
+                      end, One, [ J - M  || M <- Ms, M /= J]),
+    erlang_pbc:element_div(Num, Den).
+
+apply(LagrangePoly, J) ->
+    dkg_polynomial:apply(LagrangePoly, J).
+
+enumerate(List) ->
+    lists:zip(lists:seq(0, length(List) - 1), List).
