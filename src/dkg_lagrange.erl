31c941d081cc36f1374cb11ee0cb636163a63d7e
Rahul Garg <vihu89@gmail.com>
2018-05-31 12:47:47 -0700

    add apply function to lagrange

--- src/dkg_lagrange.erl
+++ src/dkg_lagrange.erl
@@ -1,6 +1,6 @@
 -module(dkg_lagrange).
 
--export([coefficients/3]).
+-export([coefficients/3, apply/3]).
 
 coefficients(Pairing, Indices, Alpha) ->
     lists:foldl(fun({I, Index}, Acc1) ->
@@ -14,6 +14,12 @@ coefficients(Pairing, Indices, Alpha) ->
                                           end, One, [ Idx - Index  || {J, Idx} <- enumerate(Indices), J /= I]),
                         [erlang_pbc:element_div(Num, Den) | Acc1]
                 end, [], enumerate(Indices)).
+
+apply(Pairing, Coefficients, Shares) ->
+    Zero = erlang_pbc:element_set(erlang_pbc:element_new('Zr', Pairing), 0),
+    lists:foldl(fun({Coefficient, Share}, Acc) ->
+                        erlang_pbc:element_add(Acc, erlang_pbc:element_mul(Coefficient, Share))
+                end, Zero, lists:zip(Coefficients, Shares)).
 
 enumerate(List) ->
     lists:zip(lists:seq(0, length(List) - 1), List).
