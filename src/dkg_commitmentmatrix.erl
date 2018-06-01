51f34233ccb2e718ba62cd280a604ba1bc648858
Rahul Garg <vihu89@gmail.com>
2018-06-01 15:17:50 -0700

    reverse walk the matrix, unclear why

--- src/dkg_commitmentmatrix.erl
+++ src/dkg_commitmentmatrix.erl
@@ -66,9 +66,9 @@ verify_point(Matrix, SenderID, VerifierID, Point) ->
                               R = erlang_pbc:element_pow(Acc, M),
                               Row = lists:foldl(fun(J, Acc2) ->
                                                         erlang_pbc:element_mul(erlang_pbc:element_pow(Acc2, I), lookup([II, J], Matrix))
-                                                end, G1, lists:seq(tuple_size(Matrix))),
+                                                end, G1, lists:reverse(lists:seq(1, tuple_size(Matrix)))),
                               erlang_pbc:element_mul(R, Row)
-                      end, G1, lists:seq(tuple_size(Matrix))),
+                      end, G1, lists:seq(1, tuple_size(Matrix))),
     erlang_pbc:element_cmp(Ga, Res).
 
 public_key_share(Matrix, NodeID) ->
