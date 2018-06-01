946cf2f55bee36bea7b5e93685e0b19d7ecd0c95
Rahul Garg <vihu89@gmail.com>
2018-06-01 15:18:07 -0700

    add WIP verify_point test

--- test/dkg_commitmentmatrix_test.erl
+++ test/dkg_commitmentmatrix_test.erl
@@ -12,3 +12,37 @@ verify_poly_test() ->
                               dkg_commitmentmatrix:verify_poly(CommitmentMatrix, I, Poly)
                       end, TaggedPolys)).
 
+verify_point_test() ->
+    Pairing = erlang_pbc:group_new('SS512'),
+    Secret = erlang_pbc:element_random(erlang_pbc:element_new('Zr', Pairing)),
+    RandomBiPoly = dkg_bipolynomial:generate(Pairing, 4, Secret),
+    CommitmentMatrix = dkg_commitmentmatrix:new(Pairing, RandomBiPoly),
+    TaggedPolys = [ {J, dkg_bipolynomial:apply(RandomBiPoly, erlang_pbc:element_set(erlang_pbc:element_new('Zr', Pairing), J))} || J <- lists:seq(1, 4) ],
+    Res = lists:map(fun({SenderId, Poly}) ->
+                            case dkg_commitmentmatrix:verify_poly(CommitmentMatrix, SenderId, Poly) of
+                                true ->
+                                    %% verify_poly succeeded, check verify_point for verifiers
+                                    lists:map(fun({VerifierId, Poly2}) ->
+                                                      case VerifierId /= SenderId of
+                                                          true ->
+                                                              %% verifier different than sender
+                                                              %% check verify_point
+                                                              Point = dkg_polynomial:apply(Poly2, erlang_pbc:element_set(erlang_pbc:element_new('Zr', Pairing), SenderId)),
+                                                              case dkg_commitmentmatrix:verify_point(CommitmentMatrix, SenderId, VerifierId, Point) of
+                                                                  true ->
+                                                                      %% verify_point succeeded
+                                                                      true;
+                                                                  false ->
+                                                                      false
+                                                              end;
+                                                          false ->
+                                                              false
+                                                      end
+                                              end, TaggedPolys);
+                                false ->
+                                    false
+                            end
+                    end, TaggedPolys),
+    io:format("Res: ~p~n", [Res]),
+    ?assert(false),
+    ok.
