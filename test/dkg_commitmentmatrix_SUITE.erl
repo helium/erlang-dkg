00c202b0042dca529227b9d4294be557ea63c917
Rahul Garg <vihu89@gmail.com>
2018-06-02 13:05:49 -0700

    add public_key_share_test

--- test/dkg_commitmentmatrix_SUITE.erl
+++ test/dkg_commitmentmatrix_SUITE.erl
@@ -4,10 +4,14 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -export([all/0, init_per_testcase/2, end_per_testcase/2]).
--export([verify_poly_test/1, verify_point_test/1]).
+-export([verify_poly_test/1,
+         public_key_share_test/1,
+         verify_point_test/1]).
 
 all() ->
-    [verify_poly_test, verify_point_test].
+    [verify_poly_test,
+     verify_point_test,
+     public_key_share_test].
 
 init_per_testcase(_, Config) ->
     Pairing = erlang_pbc:group_new('SS512'),
@@ -46,4 +50,25 @@ verify_point_test(Config) ->
                     end, TaggedPolys),
     io:format("Res: ~p~n", [Res]),
     ?assert(lists:all(fun(X) -> X end, lists:flatten(Res))),
+    ok.
+
+public_key_share_test(Config) ->
+    Pairing = proplists:get_value(pairing, Config),
+    Secret = erlang_pbc:element_random(erlang_pbc:element_new('Zr', Pairing)),
+    io:format("Secret: ~p~n", [erlang_pbc:element_to_string(Secret)]),
+    RandomBiPoly = dkg_bipolynomial:generate(Pairing, 5, Secret),
+    io:format("RandomBiPoly: ~p~n", [dkg_bipolynomial:print(RandomBiPoly)]),
+    CommitmentMatrix = dkg_commitmentmatrix:new(Pairing, RandomBiPoly),
+    io:format("CommitmentMatrix: ~p~n", [dkg_bipolynomial:print(CommitmentMatrix)]),
+    PublicKeySharePolynomial = [ dkg_commitmentmatrix:public_key_share(CommitmentMatrix, NodeId) || NodeId <- lists:seq(1, 6)],
+    io:format("PublicKeyShares: ~p~n", [dkg_polynomial:print(PublicKeySharePolynomial)]),
+    KnownSecret = dkg_polynomial:apply(PublicKeySharePolynomial, 0),
+    io:format("KnownSecret: ~p~n", [erlang_pbc:element_to_string(KnownSecret)]),
+    Indices = [ erlang_pbc:element_set(erlang_pbc:element_new('Zr', Pairing), I) || I <- lists:seq(1, 6) ],
+    io:format("Indices: ~p~n", [dkg_polynomial:print(Indices)]),
+    Alpha = erlang_pbc:element_set(erlang_pbc:element_new('Zr', Pairing), 0),
+    io:format("Alpha: ~p~n", [erlang_pbc:element_to_string(Alpha)]),
+    CalculatedSecret = dkg_lagrange:interpolate(PublicKeySharePolynomial, Indices, Alpha),
+    io:format("CalculatedSecret: ~p~n", [erlang_pbc:element_to_string(CalculatedSecret)]),
+    ?assert(erlang_pbc:element_cmp(KnownSecret, CalculatedSecret)),
     ok.
